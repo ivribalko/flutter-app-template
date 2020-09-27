ef0f177eb0bc33fd3b76de5074668f0da6ff77ce
Ivan Rybalko <ivribalko@gmail.com>
2020-09-27 09:43:05 -0400

    add text

--- lib/ui/app.dart
+++ lib/ui/app.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/src/model.dart';
 import 'package:get/get.dart';
 
 class App extends StatelessWidget {
+  final model = Get.find<Model>();
+
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       title: 'Flutter Demo',
       home: Scaffold(
-        appBar: AppBar(),
+        body: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Obx(() => Text('${model.count.value}')),
+              IconButton(
+                onPressed: () => model.count + 1,
+                icon: Icon(Icons.add),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
