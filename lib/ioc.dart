8b464335976ea85766de4eaec448511fcf484047
Ivan Rybalko <ivribalko@gmail.com>
2021-07-12 10:50:26 -0400

    dark mode

--- lib/ioc.dart
+++ lib/ioc.dart
@@ -1,11 +1,26 @@
 import 'package:get/get.dart';
-
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'src/common.dart';
 import 'src/model.dart';
+import 'ui/common.dart';
 
 class IoC {
-  static Future init() {
-    Get.put(Model());
+  static Future init() async {
+    await Hive.initFlutter();
 
-    return Future.value();
+    Get.put(await Hive.openBox(kSave), tag: kSave)
+        .watch(key: kDarkMode)
+        .listen(setThemeMode);
+
+    Get.put(Model());
   }
+}
+
+void setThemeMode(x) {
+  Get.changeThemeMode(
+    getThemeMode(
+      Get.find(tag: kSave),
+    ),
+  );
 }
--- lib/ui/page/page_page.dart
+++ lib/ui/page/page_page.dart
@@ -1,14 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/src/common.dart';
 import 'package:get/get.dart';
+import 'package:hive/hive.dart';
 
 class PagePage extends StatelessWidget {
+  final Box save = Get.find(tag: kSave);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: IconButton(
-          onPressed: Get.back,
-          icon: Icon(Icons.arrow_back),
+        child: Column(
+          children: [
+            IconButton(
+              onPressed: Get.back,
+              icon: Icon(Icons.arrow_back),
+            ),
+            Switch(
+              value: Get.isDarkMode,
+              onChanged: (x) => save.put(kDarkMode, !Get.isDarkMode),
+            )
+          ],
         ),
       ),
     );
